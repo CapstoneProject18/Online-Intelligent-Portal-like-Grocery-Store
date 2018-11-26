d365dc5770c254276251792415e44e228d3f3195
vs412 <vs412882@gmail.com>
2018-11-26 16:47:09 +0530

    third commit

---
+++ application/logs/log-2018-11-21.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-21 05:57:51 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 05:58:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:02:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:02:49 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:03:15 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2018-11-21 06:03:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:03:27 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:19:51 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:21:59 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2018-11-21 06:22:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-21 06:22:32 --> 404 Page Not Found: /index
---
+++ application/logs/log-2018-11-26.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-26 10:01:08 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:30 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:32 --> 404 Page Not Found: /index
+ERROR - 2018-11-26 10:33:33 --> 404 Page Not Found: /index
