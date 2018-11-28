9e5df808536c196ccbd310c2f7dbb6b727f6a3db
taniyam <33488166+taniyam@users.noreply.github.com>
2018-11-28 10:54:17 +0530

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -11,20 +11,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	::selection { background-color: #E13300; color: white; }
 	::-moz-selection { background-color: #E13300; color: white; }
 
-	body {
+	body 
+	{
 		background-color: #fff;
 		margin: 40px;
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
 
-	a {
+	a
+	{
 		color: #003399;
 		background-color: transparent;
 		font-weight: normal;
 	}
 
-	h1 {
+	h1 
+	{
 		color: #444;
 		background-color: transparent;
 		border-bottom: 1px solid #D0D0D0;
@@ -34,7 +37,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		padding: 14px 15px 10px 15px;
 	}
 
-	code {
+	code 
+	{
 		font-family: Consolas, Monaco, Courier New, Courier, monospace;
 		font-size: 12px;
 		background-color: #f9f9f9;
@@ -45,11 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		padding: 12px 10px 12px 10px;
 	}
 
-	#body {
+	#body 
+	{
 		margin: 0 15px 0 15px;
 	}
 
-	p.footer {
+	p.footer 
+	{
 		text-align: right;
 		font-size: 11px;
 		border-top: 1px solid #D0D0D0;
@@ -58,7 +64,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		margin: 20px 0 0 0;
 	}
 
-	#container {
+	#container 
+	{
 		margin: 10px;
 		border: 1px solid #D0D0D0;
 		box-shadow: 0 0 8px #D0D0D0;
